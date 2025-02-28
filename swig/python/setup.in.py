bd64d70d7a2f84d5c476509ea006ddc3beeef0b3
Børge Moe <borge.moe@met.no>
2025-02-28 11:04:32 +0100

    - Require cmake VERSION >= 3.14   - Use FindPython3 to configure python include, library and numpy dirs.   - Tidy CMakeLists.txt files

--- swig/python/setup.in.py
+++ swig/python/setup.in.py
@@ -90,6 +90,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.12',
     ],
 
     # What does your project relate to?
